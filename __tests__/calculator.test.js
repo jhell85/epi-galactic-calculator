782105387fcddc591bb5ccc4ec8c188e83522222
Joshua Hellman <jhell85@gmail.com>
2020-04-17 13:36:15 -0700

    first red of TDD for first of Age class

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -4,7 +4,9 @@ describe("Age Calculator Class tests", () => {
   let today = new Date();
   let month =  today.getMonth();
   let date = today.getDate();
-  let  tomorrowDate = todayDate;
   let fortyYearsAgo = today.getFullYear() -35;
   let fortyYearsOld = new Age(fortyYearsAgo, month, date);
+  describe("Age.getAge() should return 40 for fortyYearsOld", () => {
+    expect(fortyYearsOld.getAge()).toEqual(40);
+  });
 })
