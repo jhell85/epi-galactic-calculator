e629e94285361b7c0fdde35b4d555e9a2feb248c
Joshua Hellman <jhell85@gmail.com>
2020-04-17 15:17:24 -0700

    added 2 tests for testing age class

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,12 +1,25 @@
-import { Age } from "../src/calculator";
 
-describe("Age Calculator Class tests", () => {
+/* eslint-disable no-undef */
+import { Age } from "./../src/calculator";
+
+// These tests are written to pass based on today 35 years ago
+describe("Galactic Age Calculator app tests", () => {
   let today = new Date();
-  let month =  today.getMonth();
-  let date = today.getDate();
-  let fortyYearsAgo = today.getFullYear() -35;
-  let fortyYearsOld = new Age(fortyYearsAgo, month, date);
-  describe("Age.getAge() should return 40 for fortyYearsOld", () => {
-    expect(fortyYearsOld.getAge()).toEqual(40);
+  let todayMonth = today.getMonth() + 1;
+  let todayDate = today.getDate();
+  let tomorrowDate = todayDate + 1;
+  let thirty5YearsAgo = today.getFullYear() - 40;
+  let age = new Age(thirty5YearsAgo, todayMonth, todayDate);
+  let forty = age.getAge();
+  let ageCalculatorTomorrow  = new Age(thirty5YearsAgo,todayMonth,tomorrowDate);
+
+  describe("Age class tests", () => {
+    test("test that it should return 40 for todays date forty years ago", () => {
+      expect(forty).toEqual(40);
+    });
+    test("test should return 39 for tomorrows date forty years ago", () => {
+      let tomorrowsAge = ageCalculatorTomorrow.getAge();
+      expect(tomorrowsAge).toEqual(39);
+    });
   });
-})
+});
