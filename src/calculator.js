c5a693578cafd14ba51b3fd3a866bede60a6c31c
Joshua Hellman <jhell85@gmail.com>
2020-04-17 15:16:40 -0700

    finished class for Age Calculation

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,20 @@
-export class Age {
-    constructor(Birthyear, Birthmonth, Birthday){
-        this.year =  Birthyear;
-        this.month = Birthmonth;
-        this.day = Birthday;
-    }
-    
 
+
+export class Age {
+  constructor(birthYear, birthMonth, birthDay){
+    this.year = birthYear;
+    this.month = birthMonth;
+    this.day = birthDay;
+  }
+
+  getAge(){
+    let today = new Date();
+    let birthDate = new Date(this.year, this.month, this.day);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = (today.getMonth() + 1) - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age = age - 1;
+    }
+    return age;
+  }
 }
