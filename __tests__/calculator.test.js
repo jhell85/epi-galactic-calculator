5d676a44c97294a9c4d132e44206a128f5473dca
Joshua Hellman <jhell85@gmail.com>
2020-04-17 17:46:33 -0700

    added failing test for getMatrianYears() method

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -8,23 +8,25 @@ describe("Age Class tests", () => {
   let fortyYearsAgo = today.getFullYear() - 40;
   let forty = new Age(fortyYearsAgo, todayMonth, todayDate);
   let stillThirtyNine = new Age(fortyYearsAgo, todayMonth, tomorrowDate);
-  
-  describe("Age class tests", () => {
-    test("testing getAge() method that it created the constructor earthAge to be 40 for todays date", () => {
-      forty.getAge();
-      expect(forty.earthAge).toEqual(40);
-    });
-    test("testing getAge() method that it created the constructor earthAge to be 39 for tomorrows date", () => {
-      stillThirtyNine.getAge();
-      expect(stillThirtyNine.earthAge).toEqual(39);
-    });
-    test("testing getMercurianYears() method, that it created the constructor mercurianAge to be 166", () => {
-      forty.getMercurianYears();
-      expect(forty.mercurianAge).toEqual(166);
-    });
-    test("testing getVenusianYears() method, that it created the constructor venusianAge to be 65", () => {
-      forty.getVensianYears();
-      expect(forty.vensianAge).toEqual(166);
-    });
+
+  test("testing getAge() method that it created the constructor earthAge to be 40 for todays date", () => {
+    forty.getAge();
+    expect(forty.earthAge).toEqual(40);
+  });
+  test("testing getAge() method that it created the constructor earthAge to be 39 for tomorrows date", () => {
+    stillThirtyNine.getAge();
+    expect(stillThirtyNine.earthAge).toEqual(39);
+  });
+  test("testing getMercurianYears() method, that it created the constructor mercurianAge to be 166", () => {
+    forty.getMercurianYears();
+    expect(forty.mercurianAge).toEqual(166);
+  });
+  test("testing getVenusianYears() method, that it created the constructor venusianAge to be 64", () => {
+    forty.getVenusianYears();
+    expect(forty.venusianAge).toEqual(64);
+  });
+  test("testing getMartianYears() method, that it created the constructor martianAge to be 21", () => {
+    forty.getMartianYears();
+    expect(forty.martianAge).toEqual(21);
   });
 });
